3a688772a9262f1111b9cf8dab0944fb158a20fe
aylin <aylindeoy@gmail.com>
2023-12-09 16:42:04 +0100

    avancée test_and_set

--- test-and-set.c
+++ test-and-set.c
@@ -2,37 +2,98 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-typedef struct {
+typedef struct
+{
 	int *lock;
-}my_Lock;
+} my_Lock;
 
+// 2.1
 
-int test_and_set(my_Lock *my_Lock){
+int test_and_set(my_Lock my_lock)
+{
 	int result;
 	asm(
-		"movl $1, %%eax;"
-		"xchg %%eax, (%1);"
-		"movl %%eax, %0;"
-		
-		:"=&r"(result)
-		:"r"(my_Lock->lock)
-		:"%eax"
-	);
+			"movl $1, %%eax;"
+			"xchg %%eax, (%1);"
+			"movl %%eax, %0;"
+			:"=&r"(result)
+			:"r"(my_lock.lock)
+			:"%eax");
 	return result;
 }
 
-void unlock	(my_Lock *my_Lock){
-	my_Lock->lock = 0;
+void my_init_function(my_Lock *my_lock_ptr)
+{
+	my_lock_ptr->lock = (int *)malloc(sizeof(int));
+	*(my_lock_ptr->lock) = 0;
 }
 
-void main(){ //pas sure de ça c'est copilot qui l'a écrit 
-	my_Lock *my_Lock = malloc(sizeof(my_Lock));
-	my_Lock->lock = 0;
-	
-	printf("lock = %d\n", my_Lock->lock);
-	test_and_set(my_Lock);
-	printf("lock = %d\n", my_Lock->lock);
-	unlock(my_Lock);
-	printf("lock = %d\n", my_Lock->lock);
+void my_lock_function(my_Lock *my_lock_ptr)
+{
+	while (test_and_set(*my_lock_ptr))
+	{
+		// wait
+	}
+}
+
+void my_unlock_function(my_Lock *my_lock_ptr)
+{
+	asm(
+		"movl $0, %%eax;"
+		"xchg %%eax, (%0);"
+		:
+		: "r"(my_lock_ptr->lock)
+		: "%eax");
+}
+
+void my_destroy_function(my_Lock *my_lock_ptr)
+{
+	free(my_lock_ptr->lock);
+}
+
+// 2.4
+
+typedef struct
+{
+	my_Lock *lock_ptr;
+	int *pointer;
+} my_Sem;
+
+void my_sem_init_function(my_Sem *my_sem_ptr, int value)
+{
+	my_sem_ptr->pointer = (int *)malloc(sizeof(int));
+	*(my_sem_ptr->pointer) = value;
+
+	my_sem_ptr->lock_ptr = (my_Lock *)malloc(sizeof(my_Lock));
+}
+
+void my_sem_wait_function(my_Sem *my_sem_ptr)
+{
+	bool loop = true;
+	while (loop)
+	{
+		my_lock_function(my_sem_ptr->lock_ptr);
+		if (*(my_sem_ptr->pointer) > 0)
+		{
+			loop = false;
+			*(my_sem_ptr->pointer) -= 1;
+		}
+		my_unlock_function(my_sem_ptr->lock_ptr);
+	}
+}
+
+void my_sem_post_function(my_Sem *my_sem_ptr)
+{
+	my_lock_function(my_sem_ptr->lock_ptr);
+	*(my_sem_ptr->pointer) += 1;
+	my_unlock_function(my_sem_ptr->lock_ptr);
+}
+
+void my_sem_destroy_function(my_Sem *my_sem_ptr)
+{
+	my_destroy_function(my_sem_ptr->lock_ptr);
+	free(my_sem_ptr->lock_ptr);
+	free(my_sem_ptr->pointer);
 }
